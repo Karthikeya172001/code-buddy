99a862e7a35226acd9a841b026950df8810ae894
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-09-15 14:02:22 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-// Change this to your actual Render backend URL
+// Make sure this URL points to your deployed backend
 const API = axios.create({
   baseURL: "https://code-buddy-4mtq.onrender.com/api",
 });
 
+// Explain code
 export const explainCode = (code) => API.post("/explain", { code });
+
+// Suggest optimizations
 export const suggestOptimizations = (code) => API.post("/optimize", { code });
+
+// Generate quiz
 export const generateQuiz = (code) => API.post("/quiz", { code });
