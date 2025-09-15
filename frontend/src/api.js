f1111929a88ea14a304c1207e440f766891f199b
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-09-15 07:45:09 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://code-buddy-4mtq.onrender.com/api",
+  baseURL: "https://code-buddy-4mtq.onrender.com/api", // your backend URL
 });
 
-export const explainCode = (code) =>
-  API.post("/explain", { code });
-
-export const suggestOptimizations = (code) =>
-  API.post("/optimize", { code });
-
-export const generateQuiz = (code) =>
-  API.post("/quiz", { code });
+export const explainCode = (code) => API.post("/explain", { code });
+export const suggestOptimizations = (code) => API.post("/optimize", { code });
+export const generateQuiz = (code) => API.post("/quiz", { code });
