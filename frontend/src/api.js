de536b1da839ac81a07dc7f6cfba75b7378044d0
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-09-15 06:28:47 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,34 +1,7 @@
 import axios from "axios";
 
-// Use environment variable if available, otherwise fallback to localhost
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "https://code-buddy-4mtq.onrender.com",
+});
 
-export const explainCode = async (code) => {
-  try {
-    const res = await axios.post(`${API_URL}/explain`, { code });
-    return res.data;
-  } catch (err) {
-    console.error("Error in explainCode API:", err);
-    throw err;
-  }
-};
-
-export const suggestOptimizations = async (code) => {
-  try {
-    const res = await axios.post(`${API_URL}/suggest`, { code });
-    return res.data;
-  } catch (err) {
-    console.error("Error in suggestOptimizations API:", err);
-    throw err;
-  }
-};
-
-export const generateQuiz = async (code) => {
-  try {
-    const res = await axios.post(`${API_URL}/quiz`, { code });
-    return res.data;
-  } catch (err) {
-    console.error("Error in generateQuiz API:", err);
-    throw err;
-  }
-};
+export default API;
